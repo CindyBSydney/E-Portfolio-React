e6a904a935a24b2ad4ac32cab6cd73c3f08f1e2b
CindyBSydney <cindy.bosibori@strathmore.edu>
2024-01-27 21:28:32 +0300

    feat(Navbar): render navbar component
    Render navbar component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css'; 
 import MainSection from './components/MainSection'; 
+import NavbarSection from './components/NavbarSection';
 
 function App() {
   return (
     <div className="App">
       <MainSection /> {/* Render MainSection component */}
+      <NavbarSection /> {/* Render NavbarSection component */}
     </div>
   );
 }
