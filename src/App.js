7634705dcb56f77ad74341f6551558df2176e58d
CindyBSydney <cindy.bosibori@strathmore.edu>
2024-01-27 22:45:07 +0300

    feat: render About component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css'; 
 import MainSection from './components/MainSection'; 
 import NavbarSection from './components/NavbarSection';
+import AboutSection from './components/AboutSection';
 
 function App() {
   return (
     <div className="App">
       <MainSection /> {/* Render MainSection component */}
       <NavbarSection /> {/* Render NavbarSection component */}
+      <AboutSection /> {/* Render AboutSection component */}
     </div>
   );
 }
