567ff7000f3de05cc97ba4e07fa0a10b3c7fed02
CindyBSydney <cindy.bosibori@strathmore.edu>
2024-01-28 16:11:57 +0300

    feat(Footer): render footer component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ExperienceSection from './components/ExperienceSection';
 import ServicesSection from './components/ServicesSection'; 
 import ProjectsSection from './components/ProjectsSection';
 import ContactSection from './components/ContactSection';
+import FooterSection from './components/FooterSection';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <ServicesSection /> {/* Render ServicesSection component */}
       <ProjectsSection /> {/* Render ProjectsSection component */}
       <ContactSection /> {/* Render ContactSection component */}
+      <FooterSection /> {/* Render FooterSection component */}
     </div>
   );
 }
